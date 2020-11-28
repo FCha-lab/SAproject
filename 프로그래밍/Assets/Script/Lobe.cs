cafc799b9a2cf269800fe389beb6aa97abb52af7
jyghrtd <realize97@naver.com>
2020-11-28 15:16:37 +0900

    선택지 및 화면 전환

--- 프로그래밍/Assets/Script/Lobe.cs
+++ 프로그래밍/Assets/Script/Lobe.cs
@@ -8,8 +8,28 @@ public class Lobe : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
-	}
+
+        LobeState.Strength = 100;
+        LobeState.Mental = 100;
+        LobeState.Health = 100;
+
+        LobeState.StrengthEmpty = true;
+        LobeState.MentalEmpty = true;
+        LobeState.HealthEmpty = true;
+
+        LobeState.isDeadFlag = 0;
+        LobeState.isDead = false;
+
+        LobeState.Recovery = 100;
+        LobeState.Stability = 100;
+        LobeState.Clean = 100;
+
+        LobeState.RecoveryState = 2;
+        LobeState.StabilityState = 2;
+        LobeState.CleanState = 2;
+
+        LobeState.isFemale = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
