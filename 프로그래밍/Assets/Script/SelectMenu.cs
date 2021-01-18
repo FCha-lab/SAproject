69924ac0796434acd75d29cb11b85206deef636b
jyghrtd <realize97@naver.com>
2021-01-18 14:51:30 +0900

    그래픽 첨가 시도

---
+++ 프로그래밍/Assets/Script/SelectMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SelectMenu : MonoBehaviour {
+
+    [SerializeField] GameObject menuBar;
+
+    public void MenuOpenClose()
+    {
+        bool menuOpen = menuBar.activeSelf; //메뉴 여닫기
+        menuBar.SetActive(!menuOpen);
+    }
+
+    public void StartPage()
+    {
+        SceneManager.LoadScene("StartPage"); //이후 메뉴로 변경하면 될 항목
+    }
+
+    public void save()
+    {
+
+    }
+
+    public void load()
+    {
+
+    }
+}
---
+++ 프로그래밍/Assets/Script/StateOpen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateOpen : MonoBehaviour {
+
+    [SerializeField] GameObject statusMenu;
+
+    public string statusPoint;
+    private bool mouseClick;
+
+    /*public void OnClick() //클릭 설정 실패작
+    {
+        statusOpen();
+    }
+
+    public void statusOpen()
+    {
+        bool status = statusMenu.activeSelf; //메뉴 여닫기
+        statusMenu.SetActive(!status);
+    }*/
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*if (mouseClick)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                OnClick();
+            }
+        }*/
+	}
+}
